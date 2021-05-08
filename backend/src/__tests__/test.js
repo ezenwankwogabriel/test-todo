edfdd9665590cbbd5c706e665193f083af6fc256
Uzoma Nwoko <nwokouzomaje@hotmail.com>
2021-05-08 22:23:51 +0100

    Initial commit

---
+++ backend/src/__tests__/test.js
@@ -0,0 +1,128 @@
+const expect = require("chai").expect;
+const request = require("supertest");
+const app = require("../api");
+const mongoose = require('mongoose');
+const dburl = 'mongodb://localhost:27017/todo'
+const db = require("../database");
+
+describe("api/users=", () => {
+    beforeAll(async () => {
+        await mongoose.connect(dburl, { useUnifiedTopology: true });
+    });
+
+    afterAll(async () => {
+        mongoose.disconnect();
+    });
+
+    it("gets the test endpoint", async done => {
+        const response = await request(app).get("/");
+
+        expect(response.status).toBe(200);
+        done();
+    });
+    /* 
+        describe("GET /", () => {
+            it("should return all users", async () => {
+                const users = [
+                    { name: "george", email: "geo@gmail.com", country: "romania" },
+                    { name: "maria", email: "maria@gmail.com", country: "spain" }
+                ];
+                await User.insertMany(users);
+                console.log(users);
+                const res = await request(app).get("/api/users");
+                expect(res.status).to.equal(200);
+                expect(res.body.length).to.equal(2);
+            });
+        });
+    
+        describe("GET/:id", () => {
+            it("should return a user if valid id is passed", async () => {
+                const user = new User({
+                    name: "florian",
+                    email: "florian@gmail.com",
+                    country: "germany"
+                });
+                await user.save();
+                const res = await request(app).get("/api/users/" + user._id);
+                expect(res.status).to.equal(200);
+                expect(res.body).to.have.property("name", user.name);
+            });
+    
+            it("should return 400 error when invalid object id is passed", async () => {
+                const res = await request(app).get("/api/users/1");
+                expect(res.status).to.equal(400);
+            });
+    
+            it("should return 404 error when valid object id is passed but does not exist", async () => {
+                const res = await request(app).get("/api/users/5f43ef20c1d4a133e4628181");
+                expect(res.status).to.equal(404);
+            });
+        });
+    
+        describe("POST /", () => {
+            it("should return user when the all request body is valid", async () => {
+                const res = await request(app)
+                    .post("/api/users")
+                    .send({
+                        name: "esteve",
+                        email: "esteve@gmail.com",
+                        country: "spain"
+                    });
+                const data = res.body;
+                expect(res.status).to.equal(200);
+                expect(data).to.have.property("_id");
+                expect(data).to.have.property("name", "esteve");
+                expect(data).to.have.property("email", "esteve@gmail.com");
+                expect(data).to.have.property("country", "spain");
+                expect(data.name).to.have.length.within(3, 50);
+                expect(data.email).to.have.length.within(5, 255);
+    
+                const user = await User.findOne({ email: 'esteve@gmail.com' });
+                expect(user.name).to.equal('esteve');
+                expect(user.email).to.equal('esteve@gmail.com');
+            });
+        });
+    
+        describe("PUT /:id", () => {
+            it("should update the existing user and return 200", async () => {
+                const user = new User({
+                    name: "lola",
+                    email: "lola@gmail.com",
+                    country: "spain"
+                });
+                await user.save();
+    
+                const res = await request(app)
+                    .put("/api/users/" + user._id)
+                    .send({
+                        name: "juan",
+                        email: "juan@gmail.com",
+                        country: "spain"
+                    });
+    
+                expect(res.status).to.equal(200);
+                expect(res.body).to.have.property("name", "juan");
+                expect(res.body).to.have.property("email", "juan@gmail.com");
+                expect(res.body).to.have.property("country", "spain");
+            });
+        });
+    
+        describe("DELETE /:id", () => {
+            it("should delete requested id and return response 200", async () => {
+                const user = new User({
+                    name: "george",
+                    email: "geo@gmail.com",
+                    country: "spain"
+                });
+                await user.save();
+                userId = user._id;
+                const res = await request(app).delete("/api/users/" + userId);
+                expect(res.status).to.be.equal(200);
+            });
+    
+            it("should return 404 when deleted user is requested", async () => {
+                let res = await request(app).get("/api/users/" + userId);
+                expect(res.status).to.be.equal(404);
+            });
+        }); */
+});
