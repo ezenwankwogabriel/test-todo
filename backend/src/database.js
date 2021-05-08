c8262f0c1670fff2f92ca62ba0045264cfb8ac4c
gabby <ezenwankwogabriel@gmail.com>
2021-05-08 23:13:56 +0100

    update

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,12 +1,6 @@
-/* const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const database = module.exports;
+
 database.connect = async function connect() {
-  database.client = await MongoClient.connect('mongodb://root:1234@database/', { useUnifiedTopology: true });
+  database.client = await MongoClient.connect('mongodb://localhost:27017/', { useUnifiedTopology: true });
 };
- */
-
-const mongoose = require('mongoose');
-
-const dburl = 'mongodb://localhost:27017/todo'
-
-await mongoose.connect(dburl, { useUnifiedTopology: true });
